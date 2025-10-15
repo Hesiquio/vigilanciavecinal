aa1c1590ff65ed217382fa3f7156d3c3168643f2
Hesiquio zarate <zarate@itsescarcega.edu.mx>
2025-10-15 22:02:39 +0000

    Try fixing this error: `Build Error: Export Polygon doesn't exist in tar

--- src/components/dashboard/GoogleMap.tsx
+++ src/components/dashboard/GoogleMap.tsx
@@ -1,18 +1,45 @@
 "use client";
 
-import { APIProvider, Map, Marker, Polygon } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, useMap, AdvancedMarker } from "@vis.gl/react-google-maps";
+import { useEffect } from "react";
 
 type GoogleMapProps = {
   markerPosition?: { lat: number; lng: number };
   polygon?: { lat: number; lng: number }[];
 };
 
+
+const MapWithPolygon = ({ polygon }: { polygon?: { lat: number, lng: number }[]}) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (map && polygon) {
+      const polygonPath = new google.maps.Polygon({
+        paths: polygon,
+        strokeColor: "hsl(var(--primary))",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "hsl(var(--primary))",
+        fillOpacity: 0.35,
+      });
+      polygonPath.setMap(map);
+      
+      return () => {
+        polygonPath.setMap(null);
+      }
+    }
+  }, [map, polygon]);
+
+  return null;
+}
+
+
 export default function GoogleMap({ markerPosition, polygon }: GoogleMapProps) {
   // Center map on polygon or marker, otherwise default to Mexico City
   const center = polygon?.[0] || markerPosition || { lat: 19.4326, lng: -99.1332 };
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!} >
       <Map
         defaultCenter={center}
         defaultZoom={15}
@@ -20,17 +47,8 @@ export default function GoogleMap({ markerPosition, polygon }: GoogleMapProps) {
         disableDefaultUI={true}
         mapId="b1b2f2c2a3e4f5a6"
       >
-        {markerPosition && <Marker position={markerPosition} />}
-        {polygon && (
-          <Polygon
-            paths={polygon}
-            strokeColor="hsl(var(--primary))"
-            strokeOpacity={0.8}
-            strokeWeight={2}
-            fillColor="hsl(var(--primary))"
-            fillOpacity={0.35}
-          />
-        )}
+        {markerPosition && <AdvancedMarker position={markerPosition} />}
+        {polygon && <MapWithPolygon polygon={polygon} />}
       </Map>
     </APIProvider>
   );
