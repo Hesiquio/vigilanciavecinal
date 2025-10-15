23cea0e41a782e7e950d0141edc74cbac7cf3a47
Hesiquio zarate <zarate@itsescarcega.edu.mx>
2025-10-15 22:13:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "classroom-clash-7frrt",
+  "appId": "1:48679935869:web:53f8f20aebd41b13b57614",
+  "apiKey": "AIzaSyDfPAk4GFiB3mR6yIyW4xA5t921SjKvH3w",
+  "authDomain": "classroom-clash-7frrt.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "48679935869"
+};
