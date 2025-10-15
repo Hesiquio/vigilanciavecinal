00b81f9c14b794f7e9577a3050a6d40916316d41
Hesiquio zarate <zarate@itsescarcega.edu.mx>
2025-10-15 22:48:15 +0000

    aun no esta el espacio de perfil al darle clic en mi imagen

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -3,9 +3,10 @@
 
 import type { ReactNode } from "react";
 import React from "react";
+import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SosModal } from "./SosModal";
-import { Bell, LogOut } from "lucide-react";
+import { Bell, LogOut, User as UserIcon } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -65,6 +66,13 @@ export function AppShell({ user, onSignOut, children }: { user: User, onSignOut:
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+               <DropdownMenuItem asChild>
+                <Link href="/settings">
+                  <UserIcon className="mr-2 h-4 w-4" />
+                  <span>Ir al Perfil</span>
+                </Link>
+              </DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem onClick={onSignOut}>
                 <LogOut className="mr-2 h-4 w-4" />
