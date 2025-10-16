3cb727b27f6e887a92e6756c28d8e97a70e11ee6
Hesiquio zarate <zarate@itsescarcega.edu.mx>
2025-10-16 00:51:23 +0000

    Try fixing this error: `Console Error: [2025-10-16T00:50:47.433Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "classroom-clash-7frrt",
   "appId": "1:48679935869:web:53f8f20aebd41b13b57614",
-  "apiKey": "AIzaSyDfPAk4GFiB3mR6yIyW4xA5t921SjKvH3w",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "classroom-clash-7frrt.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "48679935869"
